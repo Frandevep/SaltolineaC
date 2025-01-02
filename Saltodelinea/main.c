f7578550c4f8e948b066cd65e6a42e605942a17d
Frandevep <frandevep@gmail.com>
2025-01-02 00:35:41 -0300

    Add files via upload

---
+++ Saltodelinea/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main()
+{
+    printf("Hola mundo,hoy es 01/01/2025\n"); //Impresion en pantalla
+    printf("\n"); //Generamos salto de linea
+    printf("Mundo, ya es 02/01/2025");
+    return 0;
+}
